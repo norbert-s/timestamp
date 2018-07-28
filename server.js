1d2c56c68ba1faae9d2fb530dfdc0cf0cd0092fa
Susztek Norbert <susztek.norbert@gmail.com>
2018-07-28 20:58:47 +0200

    perfecting

--- server.js
+++ server.js
@@ -3,27 +3,30 @@ const app = express();
 const dateFormat = require('dateformat');
 const url = require ('url');
 const cors = require('cors');
-
-
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static(__dirname + '/public'));
 app.get("/", function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
-app.get("/hello", function (req, res) {
-    res.json({greeting: 'hello API'});
-})
+
 
 //------------------------------------------------------------------
 
-
 let baseOne= app.get('/*', (req, res) =>{
-    let plusUrl= '/api/timestamp/',
-        path = req.path,
-        isCorrectPath = checkCorrPath(path,plusUrl),
-        barePath=removeSurp(path,plusUrl);
-    ifNotCorrPath(isCorrectPath,barePath,path,plusUrl,res);
+    let plusUrl= '/api/timestamp/';
+    let path = req.path;
+    let isCorrectPath = checkCorrPath(path,plusUrl);
+    let barePath=removeSurp(path,plusUrl);
+    let NumLet= onlyLetters(barePath,res);
     //there are only numbers in the string
+    if(!isCorrectPath /*&& path.length<15*/){
+        valMinPath(res,plusUrl) ;
+    }
+    if(isCorrectPath && barePath==''){
+        let now = new Date();
+        let unix = Date.parse(now);
+        res.json({unix:unix, natural:now.toUTCString()})
+    }
     let onlyNum = isNumber(barePath);
     let validDate= ifValidDate(barePath,res);
     //if barePath contains only numbers
@@ -31,19 +34,30 @@ let baseOne= app.get('/*', (req, res) =>{
     if(onlyNum){
         let unix = Number(barePath);
         let date = new Date(unix);
-        res.json({unix:unix,natural:date});
+        res.json({unix:unix,natural:date.toUTCString()});
     }
     if(validDate){
         let unix = Date.parse(trimedQueryString);
         res.json({unix:unix, natural:trimedQueryString});
     }
+    if(!validDate){
+        valMinPath(res);
+    }
 });
+let onlyLetters=(barePath,res)=>{
+    let regex = /^[a-zA-Z]+$/g;
+    if(regex.test(barePath)){
+        valMinPath(res);
+    }
+}
+let valMinPath = function(res){
+    res.json({unix:'null',utc:'invalid date'});
+}
 let ifValidDate=(barePath,res)=>{
     if(Date.parse(barePath)){
         return true;
     }
 };
-
 let isNumber=(barePath)=>{
     let regex = /^[0-9]*$/g;
     let isNum= regex.test(barePath);
@@ -53,25 +67,11 @@ let removeSurp =(path,plusUrl)=>{
     let newPath=path.slice(plusUrl.length,path.length);
     return newPath;
 };
-
 let checkCorrPath=(path,plusUrl)=>{
     let corrPath =false;
     if(path.includes(plusUrl)) corrPath= true;
     return corrPath;
 }
-
-let valMinPath =(res,plusUrl)=>{
-
-    res.json({unix:'null',utc:'invalid date'});
-}
-
-let ifNotCorrPath=(isCorrectPath,barePath,path,plusUrl,res)=>{
-    if(barePath===''){
-        let now = new Date();
-        res.send(now);
-    }
-}
-
 var listener = app.listen(process.env.PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port);
 });
