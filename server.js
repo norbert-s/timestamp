b08c66a5c24aa96ceacdb9a6cc1163acb8bb9586
Susztek Norbert <susztek.norbert@gmail.com>
2018-07-28 21:49:53 +0200

    prefecting_2

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const dateFormat = require('dateformat');
 const url = require ('url');
 const cors = require('cors');
+
+
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static(__dirname + '/public'));
 app.get("/", function (req, res) {
@@ -27,7 +29,10 @@ let baseOne= app.get('/*', (req, res) =>{
         let unix = Date.parse(now);
         res.json({unix:unix, natural:now.toUTCString()})
     }
+
+
     let onlyNum = isNumber(barePath);
+
     let validDate= ifValidDate(barePath,res);
     //if barePath contains only numbers
     var trimedQueryString = barePath.replace(/%20/g, ' ');
@@ -44,20 +49,25 @@ let baseOne= app.get('/*', (req, res) =>{
         valMinPath(res);
     }
 });
+
 let onlyLetters=(barePath,res)=>{
     let regex = /^[a-zA-Z]+$/g;
     if(regex.test(barePath)){
         valMinPath(res);
     }
 }
+
 let valMinPath = function(res){
     res.json({unix:'null',utc:'invalid date'});
 }
+
+
 let ifValidDate=(barePath,res)=>{
     if(Date.parse(barePath)){
         return true;
     }
 };
+
 let isNumber=(barePath)=>{
     let regex = /^[0-9]*$/g;
     let isNum= regex.test(barePath);
@@ -67,11 +77,14 @@ let removeSurp =(path,plusUrl)=>{
     let newPath=path.slice(plusUrl.length,path.length);
     return newPath;
 };
+
 let checkCorrPath=(path,plusUrl)=>{
     let corrPath =false;
     if(path.includes(plusUrl)) corrPath= true;
     return corrPath;
 }
+
+
 var listener = app.listen(process.env.PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port);
 });
